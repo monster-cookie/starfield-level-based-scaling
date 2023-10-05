1822992f64fd2390a46d60d06663fa15cc0bf51a
Brian DeGregorio <bdegregorio@vant4ge.com>
2023-10-04 23:44:32 -0600

    Hard coding paths for now will make a powershell script that can take options or env vars

--- Tools/compile.cmd
+++ Tools/compile.cmd
@@ -1,37 +1,23 @@
 @echo off
 
-REM The path to your steam starfield game directory 
-SET STARFIELD_GAME_DIR="D:\SteamLibrary\steamapps\common\Starfield"
-
-REM The path to your repo base directory
-SET REPO_BASE_DIR="C:\Repositories\Public\Starfield Mods\starfield-level-based-scaling"
-
-REM The path to where you used Champollion to decompile the Starfield PEX Scripts
-SET IMPORT="%STARFIELD_GAME_DIR%\Data\Scripts\Source\Base"
-
-REM The path to the Data\Scripts dir in you Starfield Game Dir
-SET OUTPUT="%STARFIELD_GAME_DIR%\Data\Scripts"
-
-REM The path to the distribution dir
-SET DISTDIR="%REPO_BASE_DIR%\Dist"
-
-SET FLAGS="%REPO_BASE_DIR%\Tools\Starfield_Papyrus_Flags.flg"
-SET TOOLSPATH="%REPO_BASE_DIR%\Tools"
-
 REM Notepad++/VSCODE needs current working directory to be where Caprica.exe is 
-cd "%TOOLSPATH%"
+cd "C:\Repositories\Public\Starfield Mods\starfield-level-based-scaling\Tools"
 
 cls
 echo ****************************************************************
 echo Caprica Starfield Compile
 echo.
 
-echo TOOLSPATH:  "%TOOLSPATH%"
-echo IMPORT:     "%IMPORT%"
-echo OUTPUT:     "%OUTPUT%"
-echo FLAGS:      "%FLAGS%"
+echo "TOOLSPATH:  %TOOLSPATH%"
+echo "IMPORT:     %IMPORT%"
+echo "FLAGS:      %FLAGS%"
+echo "DIST_DIR:   %DISTDIR%"
 echo.
 
-caprica.exe --game starfield --import "%IMPORT%" --flags "%FLAGS%" --output "%OUTPUT%" "%REPO_BASE_DIR%\Source\VPI_LevelBasedScaling.psc"
-copy /y "%OUTPUT%\VPI_LevelBasedScaling.pex" "%DISTDIR%\Data\Scripts\VPI_LevelBasedScaling.pex"
-copy /y "%REPO_BASE_DIR%\Source\LevelScalingConfig.txt" "%DISTDIR%\LevelScalingConfig.txt"
+
+del /s /q "C:\Repositories\Public\Starfield Mods\starfield-level-based-scaling\Dist\*.*"
+rmdir /s /q "C:\Repositories\Public\Starfield Mods\starfield-level-based-scaling\Dist"
+mkdir "C:\Repositories\Public\Starfield Mods\starfield-level-based-scaling\Dist"
+caprica.exe --game starfield --import "D:\SteamLibrary\steamapps\common\Starfield\Data\Scripts\Source\Base" --flags "C:\Repositories\Public\Starfield Mods\starfield-level-based-scaling\Tools\Starfield_Papyrus_Flags.flg" --output "C:\Repositories\Public\Starfield Mods\starfield-level-based-scaling\Dist\Data\Scripts" "C:\Repositories\Public\Starfield Mods\starfield-level-based-scaling\Source\VPI_LevelBasedScaling.psc"
+copy /y "C:\Repositories\Public\Starfield Mods\starfield-level-based-scaling\Dist\Data\Scripts\VPI_LevelBasedScaling.pex" "D:\SteamLibrary\steamapps\common\Starfield\Data\Scripts"
+copy /y "C:\Repositories\Public\Starfield Mods\starfield-level-based-scaling\Source\LevelScalingConfig.txt" "C:\Repositories\Public\Starfield Mods\starfield-level-based-scaling\Dist\LevelScalingConfig.txt"
