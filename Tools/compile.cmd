79b4d93d7f3e31701db9d814eb8a82cc8a6fbc7b
Brian DeGregorio <degregor@blackholeweb.com>
2023-10-09 09:28:03 -0600

    New file layout for source
    Needed as there are too many files now

--- Tools/compile.cmd
+++ Tools/compile.cmd
@@ -20,7 +20,7 @@ mkdir "C:\Repositories\Public\Starfield Mods\starfield-level-based-scaling\Dist\
 mkdir "C:\Repositories\Public\Starfield Mods\starfield-level-based-scaling\Dist\Data\SFSE\plugins\ConsoleCommandRunner"
 
 REM Deploy to Dist DIR
-Caprica.exe --game starfield --import "D:\SteamLibrary\steamapps\common\Starfield\Data\Scripts\Source\Base" --flags "C:\Repositories\Public\Starfield Mods\starfield-level-based-scaling\Tools\Starfield_Papyrus_Flags.flg" --output "C:\Repositories\Public\Starfield Mods\starfield-level-based-scaling\Dist\Data\Scripts" "C:\Repositories\Public\Starfield Mods\starfield-level-based-scaling\Source\VPI_LevelBasedScaling.psc"
-copy /y "C:\Repositories\Public\Starfield Mods\starfield-level-based-scaling\Source\VPI_LevelBasedScaling.psc" "C:\Repositories\Public\Starfield Mods\starfield-level-based-scaling\Dist\Data\Scripts\Source"
-copy /y "C:\Repositories\Public\Starfield Mods\starfield-level-based-scaling\Source\LevelScalingConfig.txt" "C:\Repositories\Public\Starfield Mods\starfield-level-based-scaling\Dist\Data\Batchfiles"
-copy /y "C:\Repositories\Public\Starfield Mods\starfield-level-based-scaling\Source\LevelScalingConfig.toml" "C:\Repositories\Public\Starfield Mods\starfield-level-based-scaling\Dist\Data\SFSE\plugins\ConsoleCommandRunner"
+Caprica.exe --game starfield --import "D:\SteamLibrary\steamapps\common\Starfield\Data\Scripts\Source\Base" --flags "C:\Repositories\Public\Starfield Mods\starfield-level-based-scaling\Tools\Starfield_Papyrus_Flags.flg" --output "C:\Repositories\Public\Starfield Mods\starfield-level-based-scaling\Dist\Data\Scripts" "C:\Repositories\Public\Starfield Mods\starfield-level-based-scaling\Source\Papyrus\VPI_LevelBasedScaling.psc"
+copy /y "C:\Repositories\Public\Starfield Mods\starfield-level-based-scaling\Source\Papyrus\VPI_LevelBasedScaling.psc" "C:\Repositories\Public\Starfield Mods\starfield-level-based-scaling\Dist\Data\Scripts\Source"
+copy /y "C:\Repositories\Public\Starfield Mods\starfield-level-based-scaling\Source\Batchfiles\LevelScalingConfig.txt" "C:\Repositories\Public\Starfield Mods\starfield-level-based-scaling\Dist\Data\Batchfiles"
+copy /y "C:\Repositories\Public\Starfield Mods\starfield-level-based-scaling\Source\CCR\LevelScalingConfig.toml" "C:\Repositories\Public\Starfield Mods\starfield-level-based-scaling\Dist\Data\SFSE\plugins\ConsoleCommandRunner"
