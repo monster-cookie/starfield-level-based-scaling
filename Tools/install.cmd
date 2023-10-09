04a08c6d71d4b6e29081510833b4a88f3b9404ce
Brian DeGregorio <degregor@blackholeweb.com>
2023-10-09 09:51:33 -0600

    Adding script for resetting config

--- Tools/install.cmd
+++ Tools/install.cmd
@@ -5,4 +5,7 @@ cd "C:\Repositories\Public\Starfield Mods\starfield-level-based-scaling\Tools"
 
 copy /y "C:\Repositories\Public\Starfield Mods\starfield-level-based-scaling\Dist\Data\Scripts\VPI_LevelBasedScaling.pex" "D:\SteamLibrary\steamapps\common\Starfield\Data\Scripts"
 copy /y "C:\Repositories\Public\Starfield Mods\starfield-level-based-scaling\Dist\Data\Batchfiles\LevelScalingConfig.txt" "D:\SteamLibrary\steamapps\common\Starfield\Data\Batchfiles"
+copy /y "C:\Repositories\Public\Starfield Mods\starfield-level-based-scaling\Dist\Data\Batchfiles\vpi-reset-vanilla.txt" "D:\SteamLibrary\steamapps\common\Starfield\Data\Batchfiles"
+copy /y "C:\Repositories\Public\Starfield Mods\starfield-level-based-scaling\Dist\Data\Batchfiles\vpi-reset-defaults.txt" "D:\SteamLibrary\steamapps\common\Starfield\Data\Batchfiles"
+copy /y "C:\Repositories\Public\Starfield Mods\starfield-level-based-scaling\Dist\Data\Batchfiles\vpi-custom-config.txt" "D:\SteamLibrary\steamapps\common\Starfield\Data\Batchfiles"
 copy /y "C:\Repositories\Public\Starfield Mods\starfield-level-based-scaling\Dist\Data\SFSE\plugins\ConsoleCommandRunner\LevelScalingConfig.toml" "D:\SteamLibrary\steamapps\common\Starfield\Data\SFSE\plugins\ConsoleCommandRunner"
