cb5257341937a873ded0025baef6031209c9aacc
Brian DeGregorio <degregor@blackholeweb.com>
2023-10-13 22:01:04 -0600

    Updated to get the new config bats into optional dist folders

--- Tools/install.cmd
+++ Tools/install.cmd
@@ -8,6 +8,5 @@ copy /y "C:\Repositories\Public\Starfield Mods\starfield-level-based-scaling\Dis
 copy /y "C:\Repositories\Public\Starfield Mods\starfield-level-based-scaling\Dist\Data\Batchfiles\vpi-lbs-reset-vanilla.txt" "D:\SteamLibrary\steamapps\common\Starfield\Data\Batchfiles"
 copy /y "C:\Repositories\Public\Starfield Mods\starfield-level-based-scaling\Dist\Data\Batchfiles\vpi-lbs-reset-defaults.txt" "D:\SteamLibrary\steamapps\common\Starfield\Data\Batchfiles"
 copy /y "C:\Repositories\Public\Starfield Mods\starfield-level-based-scaling\Dist\Optional\Balanced\Data\Batchfiles\vpi-lbs-config-balanced.txt" "D:\SteamLibrary\steamapps\common\Starfield\Data\Batchfiles"
-copy /y "C:\Repositories\Public\Starfield Mods\starfield-level-based-scaling\Dist\Optional\EncounterZoneLimits\Data\Batchfiles\vpi-lbs-config-zone-limits.txt" "D:\SteamLibrary\steamapps\common\Starfield\Data\Batchfiles"
-copy /y "C:\Repositories\Public\Starfield Mods\starfield-level-based-scaling\Dist\Optional\Hard\Data\Batchfiles\vpi-lbs-reset-config-hard.txt" "D:\SteamLibrary\steamapps\common\Starfield\Data\Batchfiles"
+copy /y "C:\Repositories\Public\Starfield Mods\starfield-level-based-scaling\Dist\Optional\Hard\Data\Batchfiles\vpi-lbs-config-hard.txt" "D:\SteamLibrary\steamapps\common\Starfield\Data\Batchfiles"
 copy /y "C:\Repositories\Public\Starfield Mods\starfield-level-based-scaling\Dist\Data\SFSE\plugins\ConsoleCommandRunner\LevelScalingConfig.toml" "D:\SteamLibrary\steamapps\common\Starfield\Data\SFSE\plugins\ConsoleCommandRunner"
